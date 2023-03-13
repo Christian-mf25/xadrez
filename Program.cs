8b8a96782d64cee69381872b971fa940fa642194
Christian <christian.mf22@gmail.com>
2023-03-13 17:13:01 -0300

    feature: Creating the Posicao class

--- Program.cs
+++ Program.cs
@@ -1,9 +1,19 @@
-﻿namespace xadrez
+﻿using tabuleiro;
+
+namespace xadrez
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
+
+			Posicao P;
+
+			P = new Posicao(3, 4);
+
+			Console.WriteLine($"Posição: {P}");
+
+			Console.ReadLine();
 		}
 	}
 }
