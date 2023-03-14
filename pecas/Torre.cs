0f1b1899d1324206f52803d486a55844aeb9302f
Christian <christian.mf22@gmail.com>
2023-03-14 09:30:35 -0300

    feature: Starting to place pieces on the board

---
+++ pecas/Torre.cs
@@ -0,0 +1,17 @@
+using tabuleiro;
+
+namespace pecas
+{
+	class Torre : Peca
+	{
+		public Torre(Tabuleiro tab, Cor cor) : base(tab, cor)
+		{
+
+		}
+
+		public override string ToString()
+		{
+			return "T";
+		}
+	}
+}
